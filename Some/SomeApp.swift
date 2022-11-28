67d8f28c92c51815142a6264785f8436feef9f8c
Daniil Davidovich <dd.jopgm@gmail.com>
2022-11-28 11:32:31 +0300

    Initial Commit

---
+++ Some/SomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeApp.swift
+//  Some
+//
+//  Created by Daniil Davidovich on 28.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
